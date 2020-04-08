47441bac534e1e4ed85a14ba18df60923be3f4d7
rafael-conrado <rafael070893@hotmail.com>
2020-04-08 18:54:03 -0300

    aula 11 e desafio 01

---
+++ Aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel} KM/H`)
+
+vel >60 ? console.log(`Você ultrapassou a velocidade permitida, MULTADO!`) : console.log(`Dirija sempre usando cinto de segurança!`) ;
+
+/* if (vel > 60){ // condição simples
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`); */
