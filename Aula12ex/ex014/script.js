2c0c75c719b768faafcaaeb3d65eb2ad88ef05b7
rafael-conrado <52204078+rafael-conrado@users.noreply.github.com>
2020-04-09 21:58:59 -0300

    Update script.js

--- Aula12ex/ex014/script.js
+++ Aula12ex/ex014/script.js
@@ -1,21 +1,21 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
+    var data = new Date() // instanciar para poder trabalhar com datas
+    var hora = data.getHours() // pega somente o valor inteiro da hora 
 
     msg.innerHTML = `Agora são ${hora} horas`
 
     if (hora >= 0 && hora < 12) {
         img.src = 'manha.png'
-        document.body.style.background = '#e2cd9f'
+        document.body.style.background = '#e2cd9f' // alteracao do plano de fundo dinamicamente
     } else if (hora >= 12 && hora <= 18) {
         img.src = 'tarde.png'
-        document.body.style.background = '#b9846f'
+        document.body.style.background = '#b9846f'// alteracao do plano de fundo dinamicamente
 
     } else {
         img.src = 'noite.png'
-        document.body.style.background = '#515154'
+        document.body.style.background = '#515154'// alteracao do plano de fundo dinamicamente
 
     }
 }
