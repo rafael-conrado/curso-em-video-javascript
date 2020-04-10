a0633abe20da6e5ac418d450e17b1dc85537d653
rafael-conrado <rafael070893@hotmail.com>
2020-04-09 22:45:55 -0300

    estrutura de repeticao while e do while

---
+++ Aula13/ambiente.js
@@ -0,0 +1,21 @@
+var c = 1
+while (c < 7) {
+    console.log('tudo bem ')
+    c++
+}
+
+do {
+    console.log('tudo bem ')
+    c++
+    
+} while (c < 7);
+
+
+
+/*console.log('tudo bem ')
+console.log('tudo bem ')
+console.log('tudo bem ')
+console.log('tudo bem ')
+console.log('tudo bem ')
+console.log('tudo bem ')
+*/
