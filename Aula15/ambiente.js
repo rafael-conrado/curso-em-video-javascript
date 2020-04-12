a73b1871b6ec3f63ce8f0c08e2a4e93fcf6a1452
rafael-conrado <rafael070893@hotmail.com>
2020-04-11 23:43:28 -0300

    aula 15 introducao  vetores

---
+++ Aula15/ambiente.js
@@ -0,0 +1,18 @@
+let num = [1,2,3,4]
+console.log(`nosso vetor é o ${num}`)
+num[1] = 9 // coloca o numero 9 na posição 1, que no caso é a segunda casa, array começa em 0
+num.push(5) // coloca um elemento na última posição
+console.log(`nosso vetor é o ${num}`)
+console.log(num.length)// tamanho do vetor
+console.log(num.sort())//ordernar elementos
+
+let pos = num.indexOf(14)// busca o valor entre() e retorna a posição no vetor, caso não seja encontrado ele retorna -1
+if (pos == -1) {
+    console.log("O valor não foi encontrado")
+} else {
+    console.log(`O valor 4 està na posição ${pos}`)
+
+}
+
+
+
---
+++ Aula15/vetornatela.js
@@ -0,0 +1,14 @@
+let valores = [7,3,9,1,2,9,5,0]
+
+/* for (let pos = 0; pos < valores.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+    
+}*/
+
+// foreach
+
+for (let pos in valores) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
+
+
